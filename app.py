d780501a86670f70e113e97a4ab086e7e9fdfaf0
harens <12570877+harens@users.noreply.github.com>
2024-06-11 12:16:52 +0100

    feat(app.py): booking already booked seat throws error

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import signal
 import os
 
 import websockets
+import mysql.connector
 
 booked_seats = set()
 flagged_seats = set()
@@ -12,17 +13,29 @@ break_seats = set()
 
 users = set()
 
+# dataBase = mysql.connector.connect(
+#   host = os.environ.get("MYSQL_HOST", "localhost"),
+#   user = os.environ.get("MYSQL_USER", "default_user"),
+#   passwd = os.environ.get("MYSQL_PASSWORD", "default_password"),
+#   db = os.environ.get("MYSQL_DB", "default_db")
+# )
 
 async def update(websocket):
     global booked_seats, flagged_seats, break_seats, users
     try:
         users.add(websocket)
         await websocket.send(f"booked: {booked_seats}, flagged: {flagged_seats}, break: {break_seats}")
+        # cur = dataBase.cursor()
         async for message in websocket:
             match message.split(" ")[0]:
                 case "book":
                     seat = int(message.split(" ")[1])
-                    booked_seats.add(seat)
+                    # If seat already booked, send error message to client
+                    if seat in booked_seats:
+                        await websocket.send("error: seat already booked")
+                        continue
+                    else:
+                        booked_seats.add(seat)
                 case "unbook":
                     seat = int(message.split(" ")[1])
                     booked_seats.remove(seat)
@@ -42,7 +55,9 @@ async def update(websocket):
                     booked_seats = set()
                     flagged_seats = set()
                     break_seats = set()
+                    # cur.execute("DELETE FROM seats")
             websockets.broadcast(users, f"booked: {booked_seats}, flagged: {flagged_seats}, break: {break_seats}")
+        # cur.close()
     finally:
         users.remove(websocket)
 
