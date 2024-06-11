1ee967eef8bf754f104340758cae84e955795fd9
harens <12570877+harens@users.noreply.github.com>
2024-06-11 11:49:22 +0100

    refactor(app.py): switch to websockets

--- app.py
+++ app.py
@@ -1,142 +1,65 @@
+#!/usr/bin/env python
+
+import asyncio
+import signal
 import os
 
-from flask import Flask, jsonify, request
-from flask_mysqldb import MySQL
+import websockets
 
-app = Flask(__name__)
+booked_seats = set()
+flagged_seats = set()
+break_seats = set()
 
-# Configure MySQL from environment variables
-app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST", "localhost")
-app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER", "default_user")
-app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD", "default_password")
-app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB", "default_db")
-
-# Initialize MySQL
-mysql = MySQL(app)
+users = set()
 
 
-@app.route("/")
-def hello():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats")
-    results = [
-        {"seat_number": seat_number, "on_break": on_break, "is_flagged": is_flagged}
-        for (seat_number, on_break, is_flagged) in cur
-    ]
-    cur.close()
-
-    return jsonify({"results": results})
-
-
-@app.route("/clear", methods=["POST"])
-def clear():
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM seats")
-    mysql.connection.commit()
-    cur.close()
-    return (
-        jsonify({"message": "All seats are now free and removed from the database"}),
-        200,
-    )
-
-
-@app.route("/claim", methods=["POST"])
-def claim():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat already claimed"}), 400
-
-    cur.execute(
-        "INSERT INTO seats (seat_number, on_break) VALUES (%s, %s)",
-        (seat_number, False),
-    )
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat claimed successfully"}), 200
+async def update(websocket):
+    global booked_seats, flagged_seats, break_seats, users
+    try:
+        users.add(websocket)
+        await websocket.send(f"booked: {booked_seats}, flagged: {flagged_seats}, break: {break_seats}")
+        async for message in websocket:
+            match message.split(" ")[0]:
+                case "book":
+                    seat = int(message.split(" ")[1])
+                    booked_seats.add(seat)
+                case "unbook":
+                    seat = int(message.split(" ")[1])
+                    booked_seats.remove(seat)
+                case "flag":
+                    seat = int(message.split(" ")[1])
+                    flagged_seats.add(seat)
+                case "unflag":
+                    seat = int(message.split(" ")[1])
+                    flagged_seats.remove(seat)
+                case "break":
+                    seat = int(message.split(" ")[1])
+                    break_seats.add(seat)
+                case "unbreak":
+                    seat = int(message.split(" ")[1])
+                    break_seats.remove(seat)
+                case "clear":
+                    booked_seats = set()
+                    flagged_seats = set()
+                    break_seats = set()
+            websockets.broadcast(users, f"booked: {booked_seats}, flagged: {flagged_seats}, break: {break_seats}")
+    finally:
+        users.remove(websocket)
 
 
-@app.route("/leave", methods=["POST"])
-def leave():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if not cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat already free"}), 400
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
-    cur.execute("DELETE FROM seats WHERE seat_number = %s", (seat_number,))
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat left successfully"}), 200
-
-
-@app.route("/break", methods=["POST"])
-def on_break():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if not cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat not found"}), 400
-
-    cur.execute("UPDATE seats SET on_break = 1 WHERE seat_number = %s", (seat_number,))
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat is now on break"}), 200
-
-
-@app.route("/unbreak", methods=["POST"])
-def off_break():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if not cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat not found"}), 400
-
-    cur.execute("UPDATE seats SET on_break = 0 WHERE seat_number = %s", (seat_number,))
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat is now off break"}), 200
-
-
-@app.route("/flag", methods=["POST"])
-def flag():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if not cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat not found"}), 400
-
-    cur.execute(
-        "UPDATE seats SET is_flagged = 1 WHERE seat_number = %s", (seat_number,)
-    )
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat is now flagged"}), 200
-
-
-@app.route("/unflag", methods=["POST"])
-def unflag():
-    seat_number = request.json.get("seat_number")
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM seats WHERE seat_number = %s", (seat_number,))
-    if not cur.fetchone():
-        cur.close()
-        return jsonify({"message": "Seat not found"}), 400
-
-    cur.execute(
-        "UPDATE seats SET is_flagged = 0 WHERE seat_number = %s", (seat_number,)
-    )
-    mysql.connection.commit()
-    cur.close()
-    return jsonify({"message": "Seat is now unflagged"}), 200
+    async with websockets.serve(
+        update,
+        host="0.0.0.0",
+        port = int(os.environ.get("PORT", 5000)),
+    ):
+        await stop
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    asyncio.run(main())
